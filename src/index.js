8327e949f5dafa4ef302c8f2e615bbdd6ac188d9
eightfeet <bajiao000000@163.com>
2019-06-21 14:06:53 +0800

    fix Number polyfill

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ if (window.Promise === undefined) {
 	throw new Error('Promise pollyfill not found.');
 }
 
+// parseFloat polyfill
+Number.parseFloat = Number.parseFloat || parseFloat;
+// parseFloat polyfill
+Number.parseInt = Number.parseInt || parseInt;
+// isFinite Polyfill
+Number.isFinite = Number.isFinite || function(value) {
+	return typeof value === "number" && isFinite(value);
+};
+
 import Message from './modules/Message';
 
 module.exports = Message;
